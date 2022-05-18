92b044f3267660490f405cafa7dab69fe1d1bfc7
supergubber <89572317+supergubber@users.noreply.github.com>
2022-05-19 03:55:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+//import Heading from "./Heading";
+//import Para from "./Para";
+//import List from "./List";
+import App from "./App";
+
+
+ReactDom.render(
+  <App></App> ,document.getElementById("root")
+);
